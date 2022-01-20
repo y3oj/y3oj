b80c0dc0d4372cb3593aec17667c25bb902774d4
memset0 <34177126+memset0@users.noreply.github.com>
2022-01-20 14:50:18 +0800

    feat(homework): userlist should be sorted by `accepted_number`

--- y3oj/routes/homework.py
+++ y3oj/routes/homework.py
@@ -1,3 +1,4 @@
+import functools
 from flask import abort
 from flask_login import current_user, login_required
 
@@ -23,10 +24,32 @@ def route_homework(homework_id, usergroup_id):
     if homework_id not in app.homework or \
             usergroup_id not in app.homework[homework_id]:
         return abort(404)
+
     homework = app.homework[homework_id][usergroup_id]
+    user_list = homework.usergroup.user_list
     if not homework.includes_user(current_user):
         return abort(403)
+
     statistics = homework.get_statistics()
+    accepted_number = [0 for _ in range(len(statistics))]
+    for i in range(len(statistics)):
+        for j in range(len(statistics[i])):
+            if statistics[i][j] is not None and \
+                    statistics[i][j].status == 'Accepted':
+                accepted_number[i] += 1
+
+    def cmp(i, j):
+        print('COMPARE', i, j)
+        return accepted_number[j] - accepted_number[i] or \
+                user_list[i].key - user_list[j].key
+
+    sorted_user_index = [i for i in range(len(user_list))]
+    sorted_user_index.sort(key=functools.cmp_to_key(cmp))
+    print('SORTED', accepted_number, sorted_user_index)
+
     return render_template('homework/homework.html',
                            homework=homework,
-                           statistics=statistics)
+                           statistics=statistics,
+                           accepted_number=accepted_number,
+                           user_list=user_list,
+                           sorted_user_index=sorted_user_index)
